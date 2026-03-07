618a3d494b034127d3355c6a7f186a4f50632956
Hadelune <hadelunee@gmail.com>
2026-03-07 17:24:23 +0800

    added intent from main to inventory log + random drawables

--- app/src/main/java/com/example/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/inventoryapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.inventoryapp;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -13,12 +18,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button button = findViewById(R.id.tempbutton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
+                openInventoryLog();
+            }
         });
+    }
+    public void openInventoryLog() {
+        Intent intent = new Intent(this, InventoryLog.class);
+        intent.putExtra("greeting", "hello");
+        startActivity(intent);
     }
 }
